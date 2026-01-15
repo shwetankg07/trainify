dbd434cd5056dad1e2afeff0b83898a0effcd265
shwetank gupta <shwetankg07@gmail.com>
2026-01-15 12:47:58 +0530

    made the routines page working

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let tempExercises = [];
+
+
+// function for all the navbars
 function loadNavbar() {
     const navHTML = `
         <nav style="background: #333; padding: 10px; margin-bottom: 20px; font-family: sans-serif;">
@@ -8,11 +12,61 @@ function loadNavbar() {
         </nav>
     `;
     
-    // Inject the HTML into the placeholder div
     document.getElementById('navbar-placeholder').innerHTML = navHTML;
 }
 
-// Run the function as soon as the page loads
 document.addEventListener('DOMContentLoaded', loadNavbar);
 
+// ---- function to just add an exercise to be showed
+function addExercise() {
+    const exerciseInput = document.getElementById('exercise-input').value;
+    const list = document.getElementById('exercise-list');
+    if (list.firstElementChild && list.firstElementChild.innerText.includes("(No exercises added yet)")) {
+        list.innerHTML = "";
+    }
+    if(exerciseInput === ""){
+    alert("please type an exercise name")
+    return;
+  }
+    tempExercises.push(exerciseInput);
+    const newExercise = document.createElement('li');
+    newExercise.innerText = exerciseInput;
+    list.appendChild(newExercise);
+    document.getElementById('exercise-input').value = "";
+}
 
+// ---- function to change the routine 
+function saveRoutine(){
+    const routineName = document.getElementById('routine-name').value;
+     let newRoutine = {
+       name: routineName,
+       exercises: tempExercises
+      };
+    if(routineName === "" || tempExercises.length === 0){
+    alert("not all input fields are filled")
+    return;
+  }
+  const routineList = JSON.parse(localStorage.getItem('myRoutines')) || [];
+  routineList.push(newRoutine);
+  localStorage.setItem('myRoutines', JSON.stringify(routineList));
+  alert("Routines Saved!");
+  document.getElementById('routine-name').value = "";
+  document.getElementById('exercise-list').innerHTML = "";
+  document.getElementById('exercise-input').value = "";
+  tempExercises = [];
+};
+
+
+// ---- get the dropdown working by fetching stuff from my routines
+function populateDropdown() {
+    const existingRoutines = JSON.parse(localStorage.getItem('myRoutines')) || [];
+    const dropdown = document.getElementById('routine-dropdown');
+    dropdown.innerHTML = '<option value="">-- Choose a Routine --</option>';
+    existingRoutines.forEach(routine => {
+        const optionHTML = `<option value="${routine.name}">${routine.name}</option>`;
+        dropdown.innerHTML += optionHTML;
+    });
+}
+if (document.getElementById('routine-dropdown')) {
+    populateDropdown();
+}
